e1e93e345a921bff937da3a54db1f98c5a0be51d
Joel Kanyi <joelkanyi98@gmail.com>
2026-08-02 12:00:24 +0300

    Professional UI: icon action toolbar with overflow and tool-window gear menu

--- plugin/src/main/kotlin/io/github/joelkanyi/peek/plugin/ui/PeekToolWindowFactory.kt
+++ plugin/src/main/kotlin/io/github/joelkanyi/peek/plugin/ui/PeekToolWindowFactory.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowEx
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener
 import com.intellij.ui.content.ContentFactory
 
@@ -15,6 +16,7 @@ internal class PeekToolWindowFactory : ToolWindowFactory, DumbAware {
         val panel = PeekPanel(project)
         val content = ContentFactory.getInstance().createContent(panel.component, null, false)
         toolWindow.contentManager.addContent(content)
+        (toolWindow as? ToolWindowEx)?.setAdditionalGearActions(panel.gearActions())
 
         project.messageBus.connect(toolWindow.disposable).subscribe(
             ToolWindowManagerListener.TOPIC,
